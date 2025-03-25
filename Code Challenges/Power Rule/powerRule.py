9987cbd1e120acc768a6155faebc5070b3abe48b
Adamimoka <adamimoka@gmail.com>
2025-03-24 23:53:26 -0400

    created base for power rule problem

---
+++ Code Challenges/Power Rule/powerRule.py
@@ -0,0 +1,6 @@
+def powerRule(text_input: str) -> int:
+    #TODO: Implement the power rule
+    return None
+
+if __name__ == "__main__":
+    powerRule(input("Enter an expression (ex. 5x^2+4x+2): "))
---
+++ Code Challenges/Power Rule/powerRuleTester.py
@@ -0,0 +1,11 @@
+import unittest
+from powerRule import powerRule
+class TestPowerRule(unittest.TestCase):
+    def test_power_rule_1(self):
+        pass
+
+    def test_power_rule_2(self):
+        pass
+    
+if __name__ == "__main__":
+    unittest.main()
